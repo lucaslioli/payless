6e12a55c1676ddcf0c428ac4ef433e300e8df646
fkrein <fkrein@inf.ufsm.br>
2017-10-19 18:07:39 -0200

    Interface adjustments on Scanner page

--- src/pages/scanner/scanner.ts
+++ src/pages/scanner/scanner.ts
@@ -20,17 +20,22 @@ export class ScannerPage {
   key: string;
   waiting: boolean = false;
   cordovaAbsent: boolean = false;
+  nfceRegistered: boolean = false;
 
-  // private url: string = 'http://payless.ecoagile.com.br';
+  private url: string = 'http://payless-api.ecoagile.com.br';
   // private url: string = 'http://localhost:8000';
-  private url: string = 'http://192.168.0.104:80';
+  // private url: string = 'http://192.168.0.104:8000';
   public dadosNFCE;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams, 
     private scanner: BarcodeScanner, 
     public http: Http, 
-    public platform: Platform) {}
+    public platform: Platform) {
+      if (!this.platform.is('cordova')) {
+        this.cordovaAbsent = true;
+      }
+    }
 
   scan(){
 
@@ -41,25 +46,30 @@ export class ScannerPage {
     if (this.platform.is('cordova')) {
 
       this.scanner.scan(this.options).then(barcodeData => {
-        this.dadosQR = barcodeData.text;
 
-        this.key = this.extractKey(this.dadosQR);
         
-        this.waiting = true;
-    
-        this.http.get(this.url + '/nfce/' + this.key)
-        .map(res => res.json())
-        .subscribe(data => {
-          this.dadosNFCE = data;
+          this.dadosQR = barcodeData.text;
+          
+          this.key = this.extractKey(this.dadosQR);
+          
+          this.waiting = true;
       
-          this.showNfceData();
-        });
+          this.http.get(this.url + '/nfce/' + this.key)
+          .map(res => res.json())
+          .subscribe(data => {
+            if(data !== "202"){
+              this.dadosNFCE = data;
+          
+              this.showNfceData();
+            } else {
+              console.log("Esta nota fiscal já está cadastrada no sistema");
+              this.nfceRegistered = true;
+            }
+          });
+        
+        
       })
 
-    } else {
-
-      this.cordovaAbsent = true;
-      
     }
     
   }
