ccb4dbcb42ba02ae52b6d7cb39243200de8c7b51
lucaslioli <lucaslima.o@outlook.com>
2017-11-15 22:26:12 -0200

    Merge 2

--- src/pages/product_detail/product_detail.ts
+++ src/pages/product_detail/product_detail.ts
@@ -10,17 +10,17 @@ import { EstablishmentPage } from '../establishment/establishment';
 })
 export class Product_detailPage {
   public product: any = {};
-  private url: string = 'http://127.0.0.1:8001';
+  private url: string = '';
 
   constructor(
   	public navCtrl: NavController,
   	public navParams: NavParams,
     public http: Http
   	) {
-	  let url = this.navParams.get('api_url');
+	  this.url = this.navParams.get('api_url');
 	  let product_id = this.navParams.get('product_id');
 
-    this.http.get(url + '/produtos/' + product_id)
+    this.http.get(this.url + '/produtos/' + product_id)
         .map(res => res.json())
         .subscribe(data => {
           this.product = data[0];
