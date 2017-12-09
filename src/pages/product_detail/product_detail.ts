e711998e13313d558ea1ae442aefc1ecb4a8404e
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2017-12-09 19:57:23 -0200

    Loader e refresher implementados na página de detalhes do produto #4

--- src/pages/product_detail/product_detail.ts
+++ src/pages/product_detail/product_detail.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { EstablishmentPage } from '../establishment/establishment';
 
@@ -15,28 +15,59 @@ export class Product_detailPage {
   constructor(
   	public navCtrl: NavController,
   	public navParams: NavParams,
-    public http: Http
-  	) {
-	  this.url = this.navParams.get('api_url');
-	  let product_id = this.navParams.get('product_id');
-
-    this.http.get(this.url + '/produtos/' + product_id)
-        .map(res => res.json())
-        .subscribe(data => {
-          this.product = data[0];
-        });
+    public http: Http,
+    public loadingCtrl: LoadingController
+  ) {
+    this.url = this.navParams.get('api_url');
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad Product_detailPage');
+    this.presentLoader();
+    this.getProductDetails();
+  }
+
+  getProductDetails(){
+    // Requests our API for product info
+    this.http.get(this.url + '/produtos/' + this.navParams.get('product_id'))
+      .map(res => res.json())
+      .subscribe(data => {
+        this.product = data[0];
+      });
   }
 
   getEstablishmentInfo(id) {
+    // Pushes establishment page to which the product belongs
     this.navCtrl.push(EstablishmentPage,
     {
       'estabelecimento_id': id,
       'api_url': this.url
     });
+  }
+
+  refreshProduct(ev: any){
+    // Refreshes product details
+    this.getProductDetails();
+
+    // Timeouts in 2 seconds if action is not completed
+    setTimeout(() => {
+      ev.complete();
+    }, 2000);
+
+  }
+
+  presentLoader(){
+    // Creates loader with custom text
+    let loader = this.loadingCtrl.create({
+      content: 'Carregando...'
+    });
+
+    // Presents loader to user
+    loader.present();
+
+    // Dismisses loader after 1.5 seconds
+    setTimeout(() => {
+      loader.dismiss();
+    }, 1500);
   }
 
 }
